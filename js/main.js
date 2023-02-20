b2a237a2f59c938beda45bfa60839d094ecdb642
ilya <ilyalogonow@gmail.com>
2023-02-20 14:57:52 +0500

    I created a function to define an operation - a floating point operation.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
 const calculator = document.querySelector('.calculator');
 const history = [];
+let tempNumber = '';
+let operationType = '';
 
 calculator.addEventListener('click', function(event) {
     const target = event.target;
     if (target.classList.contains('calculator__col')) {
-        alert(target.dataset.type)
+        const data = target.dataset.type;
+        operation(data)
+        console.log(tempNumber)
+
+
+        // tempNumber = tempNumber + data;
+        // console.log(tempNumber)
     }
 });
+/*history.push(target.innerHTML);*/
 
+
+function operation(data) {
+    if (data >= 0) {
+        operationType = 'number';
+        tempNumber = tempNumber + data;
+    } else if (data == 'float') {
+        operationType = data;
+        if (!/\./.test(tempNumber)) {
+            if (tempNumber) {
+                tempNumber = tempNumber + '.';
+            } else {
+                tempNumber = '0.';
+            }
+        }
+    }
+};
